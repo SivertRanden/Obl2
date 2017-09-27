80fcac703661db8c4740ef9b59383ce79bd54d18
siver <siver@158.37.193.157>
2017-09-27 11:25:12 +0200

    d

--- WebContent/WEB-INF/home.jsp
+++ WebContent/WEB-INF/home.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -12,6 +13,7 @@
 <a href="" >Espanol</a>
 <h1>Kaffekopper AS</h1>
 <p><img src=""></p>
+<p>${apptext.haveALook}</p>
 <p>Ta en titt på våre fantastiske heidass <a href="">Produkter</a></p>
 <p></p>
 </body>
---
+++ src/servlets/HomeServlet.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+import java.util.ResourceBundle;
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Locale locale = Locale.getDefault();
+		Locale locale = new Locale("en");
+        ResourceBundle apptexts = ResourceBundle.getBundle("apptexts_en", locale);
+		
+        request.getSession().setAttribute("apptext", apptexts);
+        
+//        Locale locale = new Locale("en");
+//        Locale locale = new Locale("es");
+		request.getRequestDispatcher("WEB-INF/home.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
